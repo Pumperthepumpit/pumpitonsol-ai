4ef350214a8e4b1181d36325cc7df4b28f30f7ec
Delivers100 <anaim.murcia@hotmail.com>
2025-08-08 03:18:16 -0400

    Remove learn section files from Git

--- pages/api/process-payment.js
+++
@@ -1,5 +0,0 @@
-// pages/api/process-payment.js
-export default async function handler(req, res) {
-  // Placeholder payment endpoint
-  res.status(200).json({ message: 'Payment processing endpoint' });
-}
